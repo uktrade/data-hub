5ff9e5e4fc8b56c017c880787cfef4e69971120e
bmcorser <bmcorser@gmail.com>
2016-09-05 22:50:17 +0100

    Create CDMS tables in postgres container

--- postgres-namedatalen/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chown -R postgres:postgres ${PGDATA}
-if [ ! -e ${PGDATA}/PG_VERSION ]; then
-    su postgres -c /usr/local/pgsql/bin/initdb ${PGDATA}
-fi
-
-su postgres -c "/usr/local/pgsql/bin/pg_ctl -D ${PGDATA} -o \"-c listen_addresses='localhost'\" -w start"
