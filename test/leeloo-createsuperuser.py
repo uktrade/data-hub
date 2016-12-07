97b89a88d34eab25dcd4ab20583e48d1bdd0388d
bmcorser <bmcorser@gmail.com>
2016-12-07 11:39:51 +0000

    Update to track leeloo changes

--- test/leeloo-createsuperuser.py
+++ test/leeloo-createsuperuser.py
@@ -5,16 +5,16 @@ sys.path.append(os.getcwd())
 
 import django
 django.setup()
-from datahub.company.models import Team
-from django.contrib.auth.models import User
+from datahub.company.models import Advisor
+from datahub.metadata.models import Team
 
 username = os.environ['TEST_LEELOO_SUPERUSER_USERNAME']
 password = os.environ['TEST_LEELOO_SUPERUSER_PASSWORD']
 
 try:
-    test_user = User.objects.get(username=username)
-except User.DoesNotExist:
-    test_user = User(username=username)
+    test_user = Advisor.objects.get(email=username)
+except Advisor.DoesNotExist:
+    test_user = Advisor(email=username)
     test_user.set_password(password)
     test_user.is_superuser = True
     test_user.is_staff = True
