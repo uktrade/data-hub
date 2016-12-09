ccd688a264455874ee99c7a794e08c20233be1ec
bmcorser <bmcorser@gmail.com>
2016-12-09 02:04:22 +0000

    Know things

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,7 @@ set -e
 
 cp -r test leeloo
 echo "ADD ./test /test" >> leeloo/Dockerfile
+cat leeloo/Dockerfile
 
 trap 'docker-compose -f test-browser.yml down --remove-orphans --volumes' EXIT
 docker-compose -f test-browser.yml up --build
