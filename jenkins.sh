82d4498846e40023f6e740009190ff9fd6c266c9
bmcorser <bmcorser@gmail.com>
2016-12-09 08:18:54 +0000

    Drop first

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 set -e
 
 trap 'docker-compose -f test-browser.yml down --remove-orphans --volumes' EXIT
+docker-compose -f test-browser.yml down --remove-orphans --volumes
 docker-compose -f test-browser.yml up --build
 cd $WORKSPACE/test/browser
 ./run-outer.sh xvfb-run
