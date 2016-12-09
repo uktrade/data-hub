24352553180bb2610f36977957c8f8054772246d
bmcorser <bmcorser@gmail.com>
2016-12-09 08:15:28 +0000

    Revert hackery

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,5 @@
 set -e
 
-cp -r test leeloo
-ls -la leeloo/test
-echo "ADD ./test /test" >> leeloo/Dockerfile
-cat leeloo/Dockerfile
-
 trap 'docker-compose -f test-browser.yml down --remove-orphans --volumes' EXIT
 docker-compose -f test-browser.yml up --build
 cd $WORKSPACE/test/browser
