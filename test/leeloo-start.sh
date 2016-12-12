ae3ec6c95bad5030c6630b47cda849ab6e4f1938
Bowen Sun <bowensun@gmail.com>
2016-12-12 17:03:32 +0000

    fix leeloo path

--- test/leeloo-start.sh
+++ test/leeloo-start.sh
@@ -3,6 +3,6 @@ python /app/leeloo/manage.py migrate
 python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/metadata.yaml
 python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/undefined.yaml
 pushd /app/leeloo
-python /test/leeloo-setup.py
+python /app/test/leeloo-setup.py
 popd
 /app/start.sh
