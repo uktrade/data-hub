69e6b998a666883f4d42728927fd17888e0b0480
bmcorser <bmcorser@gmail.com>
2016-12-12 14:33:34 +0000

    Update jenkins.sh to wait for front end

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,13 @@ set -e
 
 trap 'docker-compose -f test-browser.yml down --remove-orphans --volumes' EXIT
 docker-compose -f test-browser.yml down --remove-orphans --volumes
-docker-compose -f test-browser.yml up --build
+docker-compose -f test-browser.yml up -d --build
+printf 'Waiting for front end to become available '
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+    printf '.'
+    sleep 1
+done
+echo !
 cd $WORKSPACE/test/browser
 ./run-outer.sh xvfb-run
+cd $WORKSPACE
