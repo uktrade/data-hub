6bbf0ca5e3efd4d0418007e96ea46e03fe4e8eaa
Bowen Sun <bowensun@gmail.com>
2016-12-13 12:08:56 +0000

    fix jenkins waiting

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ trap 'docker-compose -f test-browser.yml down --remove-orphans --volumes' EXIT
 docker-compose -f test-browser.yml down --remove-orphans --volumes
 docker-compose -f test-browser.yml up -d --build
 printf 'Waiting for front end to become available '
-until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+until $(curl --output /dev/null --silent --head --fail http://$(awk 'BEGIN{split(ENVIRON["DOCKER_HOST"],X,"//"); split(X[2],Y,":"); print Y[1]}'):3000); do
     printf '.'
     sleep 1
 done
