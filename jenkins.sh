6fb8e51ae828a401fc336e19b046d7ecead38c37
Bowen Sun <bowensun@gmail.com>
2016-12-14 12:59:17 +0000

    more output

--- jenkins.sh
+++ jenkins.sh
@@ -2,8 +2,8 @@ set -e
 
 trap 'cd $WORKSPACE && docker-compose -f test-browser.yml down --remove-orphans --volumes' EXIT
 echo "Bringing outer containers up ..."
-docker-compose -f test-browser.yml down --remove-orphans --volumes > /dev/null 2>&1
-docker-compose -f test-browser.yml up -d --build > /dev/null 2>&1
+docker-compose -f test-browser.yml down --remove-orphans --volumes
+docker-compose -f test-browser.yml up -d --build
 printf 'Waiting for front end to become available '
 until $(curl --output /dev/null --silent --head --fail http://$(awk 'BEGIN{split(ENVIRON["DOCKER_HOST"],X,"//"); split(X[2],Y,":"); print Y[1]}'):3000); do
     printf '.'
