8570fa739ce3556cd089fcda1b819a53af253f48
bmcorser <bmcorser@gmail.com>
2016-12-14 08:34:10 +0000

    Set user to is_active, share all the secrets

--- test/leeloo-setup.py
+++ test/leeloo-setup.py
@@ -12,18 +12,22 @@ from oauth2_provider.models import AccessToken, Application, Grant, RefreshToken
 
 username = os.environ['CDMS_USERNAME']
 password = os.environ['CDMS_PASSWORD']
+client_secret = os.environ['API_CLIENT_SECRET']
+client_id = os.environ['API_CLIENT_ID']
 
 try:
     test_user = Advisor.objects.get(email=username)
 except Advisor.DoesNotExist:
     test_user = Advisor(
-        first_name='Test',
-        last_name='User',
         email=username,
     )
+
+test_user.first_name = 'Test'
+test_user.last_name = 'User'
 test_user.set_password(password)
 test_user.is_superuser = True
 test_user.is_staff = True
+test_user.is_active = True
 test_user.dit_team = Team.objects.get(name='London International Trade Team')
 test_user.save(as_korben=True)
 
@@ -31,20 +35,12 @@ APPLICATION_NAME = 'Browser tests'
 try:
     app = Application.objects.get(name=APPLICATION_NAME)
 except Application.DoesNotExist:
-    app = Application(
-        user=test_user,
-        client_type=Application.CLIENT_CONFIDENTIAL,
-        authorization_grant_type=Application.GRANT_PASSWORD,
-        name=APPLICATION_NAME,
-    )
-    app.save()
+    app = Application()
 
-
-if len(sys.argv) > 1:
-    print("API_ROOT={0}".format('http://leeloo:8000'))
-    print("REDIS_HOST={0}".format('redis-rhod'))
-    print("REDIS_PORT={0}".format('6379'))
-    print("REDIS_URL={0}".format('redis://:@redis-rhod:6379'))
-    print("API_CLIENT_ID={0}".format(app.client_id))
-    print("API_CLIENT_SECRET={0}".format(app.client_secret))
-    print("SESSION_SECRET={0}".format('abc123'))
+app.user = test_user
+app.client_type = Application.CLIENT_CONFIDENTIAL
+app.client_id = client_id
+app.client_secret = client_secret
+app.authorization_grant_type = Application.GRANT_PASSWORD
+app.name = APPLICATION_NAME
+app.save()
--- test/leeloo-start.sh
+++ test/leeloo-start.sh
@@ -2,7 +2,8 @@
 python /app/leeloo/manage.py migrate
 python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/metadata.yaml
 python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/undefined.yaml
+python /app/leeloo/manage.py collectstatic --noinput
 pushd /app/leeloo
 python /app/test/leeloo-setup.py
-./start.sh
+python /app/leeloo/manage.py runserver 0.0.0.0:8000
 popd
